5e325af0d0ca2d95e3bf15e188687e20c43ed83a
Rafaell Kruz <80438200+Rafaellkruz@users.noreply.github.com>
2024-08-05 23:39:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function converterParaFahrenheit() {
+  const celsius = document.getElementById('celsius').value
+  const fahrenheit = (celsius * 9/5) + 32;
+  document.getElementById('fahrenheit').value = fahrenheit;
+}
+
+function converterParaCelsius() {
+  const fahrenheit = document.getElementById('fahrenheit').value;
+  const celsius = (fahrenheit - 32) * 5/9;
+  document.getElementById('celsius').value = celsius;
+}
